5ba04a14415e9b6d92becc7a2f98e650b2440ec2
jkjh8 <jkjh82@naver.com>
2020-10-22 00:47:21 +0900

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar flat app color="white">
+      <v-spacer/>
+      <div class="text-overline font-weight-light" >
+        {{ ipaddress }}
+      </div>
+    </v-app-bar>
+    <v-main app >
+      <qrcode-vue :value="value" :size="size" level="H"></qrcode-vue>
+    </v-main>
+    <v-footer app color="white" class="text-overline font-weight-light">
+      <v-row align="center" justify="center">
+        <v-img        
+        src='./assets/logo1.png'
+        max-width="15"
+        class="mr-2"
+      ></v-img>
+        Media Consulting
+      </v-row>
+      
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import myip from 'quick-local-ip'
+import QrcodeVue from 'qrcode.vue'
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    QrcodeVue
+  },
+
+  data: () => ({
+    value: 'http://' + myip.getLocalIP4(),
+    size: 200,
+    // ipaddress: 'http://' + window.location.hostname
+    ipaddress: 'http://' + myip.getLocalIP4()
+  })
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: fixed;
+  top: 40%;
+  left: 47%;
+  margin-top: -50px;
+  margin-left: -45px;
+  width: 200px;
+  height: 200px;
 }
+
 </style>
